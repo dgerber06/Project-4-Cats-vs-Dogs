7f7daa427424ab610cbbb2fdb6cd8525d47130b3
ShadowPikmin <jdcarter432@gmail.com>
2026-04-22 21:17:49 -0500

    Commits rought CNN model idea

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -13,4 +13,19 @@ model = Sequential()
 
 # ** ADD YOUR CODE HERE **
 
+filter_size = 9
+filter_num = 64
+input_size = (28,28,1)
+pool_size = (2,2) 
+
+
+model.add(Conv2D(filter_num, filter_size, input_shape=input_size, activation='relu', name="layer1")
+model.add(MaxPooling2D(pool_size=pool_size)
+model.add(Conv2D(filter_num, filter_size, input_shape=input_size, activation='relu', name="layer2")
+model.add(MaxPooling2D(pool_size=pool_size)
+model.add(Flatten())
+model.add(Dense(4, name="full_connected_layer1"))
+model.add(Dense(2, name="full_connceted_layer2"))
+
+
 
