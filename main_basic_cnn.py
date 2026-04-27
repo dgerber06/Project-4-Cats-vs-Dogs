77c692eabc803130ecbf3b0dd2db59ce8f360733
ShadowPikmin <jdcarter432@gmail.com>
2026-04-27 12:24:57 -0500

    Completed Model implementation

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -7,25 +7,27 @@ from utils import train_test_split
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 model = Sequential()
 
 # ** ADD YOUR CODE HERE **
+filter_size = (3,3) 
+filter_num = 32
+input_size = (180, 180)
+pool_size = (2,2)
 
-filter_size = 9
-filter_num = 64
-input_size = (28,28,1)
-pool_size = (2,2) 
+model.add(Conv2D(filter_num, filter_size, activation='relu'))
+model.add(MaxPooling2D(pool_size = pool_size))
 
+model.add(Conv2D(filter_num, filter_size, activation='relu'))
+model.add(MaxPooling2D(pool_size = pool_size))
 
-model.add(Conv2D(filter_num, filter_size, input_shape=input_size, activation='relu', name="layer1")
-model.add(MaxPooling2D(pool_size=pool_size)
-model.add(Conv2D(filter_num, filter_size, input_shape=input_size, activation='relu', name="layer2")
-model.add(MaxPooling2D(pool_size=pool_size)
 model.add(Flatten())
-model.add(Dense(4, name="full_connected_layer1"))
-model.add(Dense(2, name="full_connceted_layer2"))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(10, activation="sigmoid"))
 
 
+print("Model compiles")
+print(model) 
 
