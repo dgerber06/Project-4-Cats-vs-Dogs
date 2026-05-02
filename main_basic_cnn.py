e1041f3fa5251c16d79c0605cde1b29d7578b8b5
gconner-code <gconner@trinity.edu>
2026-05-02 09:59:34 -0500

    Update main_basic_cnn.py

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -42,14 +42,14 @@ train_datagen = ImageDataGenerator(rescale = 1./255)
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 train_set = train_datagen.flow_from_directory(
-  'Dataset/Train',
+  'data/Train',
   target_size = input_size,
   batch_size = 32,
   class_mode = 'binary'
 )
 
 test_set = test_datagen.flow_from_directory(
-  'Dataset/Test',
+  'data/Test',
   target_size = input_size,
   batch_size = 32,
   class_mode = 'binary'
