0df34ed75c937db338cc091a4e12fe27282b640c
gconner-code <gconner@trinity.edu>
2026-05-04 19:50:46 -0500

    Update main_basic_cnn.py

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -24,7 +24,7 @@ model.add(Conv2D(128, (3,3), activation='relu'))
 model.add(MaxPooling2D(2, 2))
 
 model.add(Flatten())
-model.add(Dense(64, activation="relu"))
+model.add(Dense(128, activation="relu"))
 model.add(Dropout(0.25))
 model.add(Dense(1, activation="sigmoid"))
 
