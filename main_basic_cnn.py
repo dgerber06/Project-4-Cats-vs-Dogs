60fa202f12d18499c60c5c7574b7f1665057b162
gconner-code <gconner@trinity.edu>
2026-05-04 19:26:28 -0500

    Update main_basic_cnn.py

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -55,8 +55,8 @@ test_set = test_datagen.flow_from_directory(
 
 model.fit(
   train_set,
-  steps_per_epoch = 150,
-  epochs = 15,
+  steps_per_epoch = 100,
+  epochs = 20,
   validation_data = test_set,
   validation_steps = 50
 )
