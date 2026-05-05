e53faac9a6d47ef4d062eec7ed77f98283dc583a
gconner-code <gconner@trinity.edu>
2026-05-04 21:33:37 -0500

    Update main_basic_cnn.py

--- main_basic_cnn.py
+++ main_basic_cnn.py
@@ -3,7 +3,7 @@ import random
 import warnings
 warnings.filterwarnings("ignore")
 from utils import train_test_split
-
+ 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dropout, Flatten, Dense
